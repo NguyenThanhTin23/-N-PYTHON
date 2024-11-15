d9456db7a745d976bd66a5d50a3f20f88f6337ce
ChiTrungVo <vochitrung212@gmail.com>
2024-11-15 07:40:43 +0700

    Creat.py

--- CRUD/Creat.py
+++ CRUD/Creat.py
@@ -1,17 +1,5 @@
 import pandas as pd
-def creat(file_path):
-    #Nhập thông tin dòng mới
-    Date = input('Nhập date (yyyy-mm-dd): ')
-    Country_Region = input('Nhập Country/Region: ')
-    Confirmed = int(input('Nhập số lượng ca dương tính đã được xác nhận (Confirmed): '))
-    Deaths = int(input('Nhập số lượng ca tử vong (Deaths): '))
-    Recovered = int(input('Nhập số ca đã khỏi bệnh (Recovered): '))
-    Active = Confirmed-Deaths-Recovered
-    New_cases = int(input('Nhập số lượng trường hợp mới (New cases): '))
-    New_deaths = int(input('Nhập số lượng ca tử vong mới (New deaths): '))
-    New_recovered = int(input('Nhập số lượng ca dương tính mới(New recovered): '))
-    WHO_Region = input('Nhập WHO_Region: ')
-    new = [Date,Country_Region,Confirmed,Deaths,Recovered,Active,New_cases,New_deaths,New_recovered,WHO_Region]
+def creat(file_path,new):
     #Đọc dữ liệu file csv vào DataFrame
     df=pd.read_csv(file_path)
     #Tạo dòng mới
