9f4bb37b8558b920342bbd261e66815674c141e3
ChiTrungVo <vochitrung212@gmail.com>
2024-11-15 07:13:03 +0700

    Read.py

--- CRUD/Read.py
+++ CRUD/Read.py
@@ -1,8 +1,4 @@
-import csv
-def read():
-    with open('full_grouped.csv','r') as file:
-        reader=csv.reader(file)
-        for row in reader:
-            print(row)
-if __name__=="__main__":
-    read()
+import pandas as pd
+def read(file_path):
+    df=pd.read_csv(file_path)
+    return df
