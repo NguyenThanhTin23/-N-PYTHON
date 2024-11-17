002da50ad84097839940050ac38a6b12f360f697
ChiTrungVo <vochitrung212@gmail.com>
2024-11-17 16:31:18 +0700

    Creat.py

--- CRUD/Creat.py
+++ CRUD/Creat.py
@@ -1,5 +1,14 @@
 import pandas as pd
 def creat(file_path,new):
+    """
+    Chức năng:
+    Thêm một dòng mới vào file CSV
+
+    Tham số:
+    file_path(str): Đường dẫn file CSV cần xử lý
+    new(list): Danh sách các giá trị của mỗi tường dữ liệu:
+            [Date, Country/Region, Confirmed, Deaths, Recovered, Active, New_cases, New_deaths, New_recovered, WHO_Region].
+    """
     #Đọc dữ liệu file csv vào DataFrame
     df=pd.read_csv(file_path)
     #Tạo dòng mới
