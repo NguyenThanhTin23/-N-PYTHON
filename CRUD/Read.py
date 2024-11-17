b5c9391ebf8f00d6939fccdb68565b0142dd448e
ChiTrungVo <vochitrung212@gmail.com>
2024-11-17 16:27:43 +0700

    Read.py

--- CRUD/Read.py
+++ CRUD/Read.py
@@ -1,4 +1,11 @@
 import pandas as pd
 def read(file_path):
+    """
+    Chức năng:
+    Đọc dữ liệu file CSV
+
+    Tham số:
+    file_path(str): Đường dẫn file CSV cần đọc
+    """
     df=pd.read_csv(file_path)
     return df
